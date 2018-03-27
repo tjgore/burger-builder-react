53ee06148e0543e6964d0b73ddad92fa34d931f6
Tyndale Gore <gorejrtyndale1@gmail.com>
2018-03-27 23:24:43 +0800

    Axios error handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-order';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -48,9 +49,9 @@ class BurgerBuilder extends Component {
   }
 
    purchaseContinueHandler = () => {
-    this.setState({purchasing: false});
-    alert('You continued!!');
-   /* this.setState({loading: true})
+    /*this.setState({purchasing: false});
+    alert('You continued!!');*/
+    this.setState({loading: true})
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -73,7 +74,7 @@ class BurgerBuilder extends Component {
     .catch(err => { 
       console.log(err);
       this.setState({ loading: false, purchasing: false })
-    });*/
+    });
   }
 
   addIngredientHandler = (type) => {
@@ -143,4 +144,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
